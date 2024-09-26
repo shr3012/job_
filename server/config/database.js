697726165e5c789f55b4b5371924ad4528ff4ff5
sherry_895 <shreyasuman9781@gmail.com>
2024-09-26 10:49:20 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -7,6 +7,10 @@ const databaseConnection = () => {
     }).then((data)=>{
         console.log(`database connected successfully at server ${data.connection.host}`)
     })
+    .catch((err) => {
+        console.error(`Failed to connect to the database. Error: ${err.message}`);
+        process.exit(1); // Exit the process with failure
+    });
 }
 
 module.exports = databaseConnection
